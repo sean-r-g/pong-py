df9bb31e2bc2415a0c38fce38f36bb3213c9d714
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-07-25 14:50:00 -0500

    basic pong setup complete

---
+++ pong.py
@@ -0,0 +1,30 @@
+import turtle
+TK_SILENCE_DEPRECATION=1
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Player paddle
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+# Computer paddle
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# Game loop
+while True:
+    wn.update()
